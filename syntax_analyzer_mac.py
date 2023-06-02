53262bc0fa3fd9d864ab3a3345d0090a5ac2bb95
realisshomyang <realisshoman@kakao.com>
2023-06-02 15:29:41 +0900

    오류메시지 개선

--- syntax_analyzer_mac.py
+++ syntax_analyzer_mac.py
@@ -4,11 +4,17 @@ from anytree.exporter import UniqueDotExporter
 from anytree import Node, RenderTree
 import os
 
+#get CFG information, LR Table information in table.py
+#lr_table -> key(state, next character of splitter) : value(actions)
+#production -> key(lefthandside production) : value(sum of non-terminals and terminals count)
 lr_table = table.lr_table
 productions = table.productions
 
+#list that save the node of the parse tree
 parsetree = []
 
+#class Treenode to represent a node in the parse tree
+#We use anytree Library
 class TreeNode:
     def __init__(self, data):
         self.data = data
@@ -24,12 +30,14 @@ class TreeNode:
             child_node.parent = node
         return node
 
+#main feature function
+#do same action with slr parser 
 def parse_lr_table(lr_table):
-    last_parse = 0
     stack = []
-    stack.append(0)  # 초기 상태(0)를 스택에 푸시
-    i = 0  # 입력 문자열의 인덱스
-    cnt = 0
+    stack.append(0)  # push the initial state (0) to the stack
+    i = 0  # index of the input string -> do a role of splitter
+    j = 0
+    cnt = 0 #variable to represent parsing step
     while True:
         cnt += 1
         state = stack[-1]
@@ -37,69 +45,87 @@ def parse_lr_table(lr_table):
         if (state, symbol) in lr_table:
             action = lr_table[(state, symbol)]
             if action[0] == 's':
-                # Shift 액션일 경우
-                stack.append(int(action[1:]))  # 다음 상태를 스택에 푸시
-                i += 1  # 다음 입력 문자열로 이동
+                # Shift action
+                stack.append(int(action[1:]))  # push the next state to the stack
+                i += 1  # move to the next input symbol
+                j+=1
             elif action[0] == 'g':
-                # Goto 액션일 경우
-                stack.append(int(action[1:]))  # 다음 상태를 스택에 푸시
+                # Goto action
+                stack.append(int(action[1:]))  # push the next state to the stack
             elif action[0] == 'r':
-                # Reduce 액션일 경우
+                # Reduce action
                 reduction = productions[int(action[1:])][0]
                 count = productions[int(action[1:])][1]
                 tmp_node = TreeNode(reduction)
                 for j in range(count):
                     stack.pop()
-                for x in parsetree[i - count : i]:
+                for x in parsetree[i - count: i]:
                     tmp_node.add_child(x)
-                last_parse = tmp_node
                 parsetree.append(tmp_node)
-                del parsetree[i - count : i]
+                del parsetree[i - count: i]
                 parsetree.insert(i - count, tmp_node)
                 stack.append(int(lr_table[stack[-1], reduction][1:]))
                 i = i - count + 1
+                j+=1
             elif action == 'acc':
-                # Accept 액션일 경우
+                # Accept action
                 return True
         else:
+            # Parsing error
             print("!!!Current parsing status!!!")
+            print("Parsing is not possible in this situation.")
             for x in parsetree:
-                if x.data == '$' :
+                if x.data == '$':
                     break
-                print(x.data , end=" ")
+                print(x.data, end=" ")
             
-            print("\nLine number(in Parsing sequence) : " + str(cnt))
-            print("current spliter position : " + str(i))
-            print("state : " + str(state) + " and next symbol : " + symbol)
+            print("\nLine number (in Parsing sequence): " + str(cnt))
+            print("Current splitter position: " + str(i))
+            print("State: " + str(state) + " and next symbol: " + symbol)
+            print("next input expected for {} is ->".format(parsetree[i-1].data) , end =" ")
+            result = [key[1] for key in lr_table.keys() if key[0] == state]
+            for x in result:
+                print(x, end=" ")
+            print(" not {}".format(symbol))
             return False
 
-# 커맨드 라인 인자로 입력 파일 경로 받기
+# Check if the input file path is provided as a command-line argument
 if len(sys.argv) < 2:
-    print("Please specify input file \n")
+    print("Please specify an input file.\n")
     sys.exit(1)
 input_file = sys.argv[1]
-# txt 파일 읽기
-with open(input_file, "r") as file:
-    file_contents = file.read()
 
-# 문자열 분리
+# Read the contents of the text file
+try:
+    with open(input_file, "r") as file:
+        file_contents = file.read()
+except FileNotFoundError:
+    print("Input file not found.")
+    sys.exit(1)
+
+# Split the input string
 input_string = file_contents.split()
 input_string.append('$')
 
+# Create tree nodes for each symbol in the input string
 for x in input_string:
     parsetree.append(TreeNode(x))
+
+# Perform LR parsing using the LR table
 result = parse_lr_table(lr_table)
+
 if result:
-    print("parsing available.")
+    print("Parsing available.")
     print("Print parse tree:")
     root = parsetree[-1].create_tree_node()
-    # 트리 그래프 생성 및 출력
+    
+    # Print the parse tree structure using indentation
     for pre, _, node in RenderTree(root):
         print(f"{pre}{node.name}")
-    print("if you open !!parsetree.png!! in this directory \nyou can see more visualized parsetree from your input")
-    #image 파일 생성
+    
+    print("If you open 'parsetree.png' in this directory, you can see a more visualized parse tree from your input.")
+    
+    # Export the parse tree as an image file
     exporter = UniqueDotExporter(root)
     exporter.to_picture("parsetree.png")
     os.system("open parsetree.png")
-else:
-    print("이 상황에서는 파싱 불가능합니다.")
